7309c835d8d43035431ab808b7a3f7b9918a49f8
Johannes Wienke <languitar@semipol.de>
2017-11-19 14:45:40 +0000

    New upstream version 0.6

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(
+    name='autosuspend',
+    version='0.6',
+
+    description='A daemon to suspend your server in case of inactivity',
+    author='Johannes Wienke',
+    author_email='languitar@semipol.de',
+    license='GPL2',
+
+    zip_safe=False,
+
+    setup_requires=[
+        'pytest-runner',
+    ],
+    install_requires=[
+        'psutil',
+    ],
+    extras_require={
+        'Mpd': ['python-mpd2'],
+        'Kodi': ['requests'],
+        'XPath': ['lxml', 'requests'],
+    },
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-mock',
+    ],
+
+    scripts=[
+        'autosuspend.py'
+    ],
+    data_files=[
+        ('etc', ['autosuspend.conf',
+                 'autosuspend-logging.conf']),
+        ('lib/systemd/system', ['autosuspend.service',
+                                'autosuspend-detect-suspend.service'])
+    ]
+)
